132b6b871fe6efdfda98465b52f054b6ccdd408c
nitinchandrol <ntnchandrol@gmail.com>
2014-11-22 11:16:15 +0530

    settings changes

---
+++ netyap/chat/migrations/0001_initial.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='chat',
+            fields=[
+                ('chat_id', models.CharField(max_length=100, serialize=False, primary_key=True)),
+                ('user_id', models.CharField(max_length=100)),
+                ('message', models.CharField(max_length=500)),
+                ('time_stamp', models.DateTimeField(auto_now_add=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='chatroom',
+            fields=[
+                ('title', models.CharField(max_length=100)),
+                ('prof', models.CharField(max_length=100)),
+                ('course_id', models.CharField(max_length=100)),
+                ('chatroom_id', models.CharField(max_length=100, serialize=False, primary_key=True)),
+                ('time_stamp', models.DateTimeField(auto_now_add=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='notice',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('notice_id', models.CharField(max_length=100)),
+                ('message', models.CharField(max_length=500)),
+                ('time_stamp', models.DateTimeField(auto_now_add=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='chat',
+            name='chatroom_id',
+            field=models.ForeignKey(to='chat.chatroom'),
+            preserve_default=True,
+        ),
+    ]
--- netyap/netyap/settings.py
+++ netyap/netyap/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'chat',
 )
 
 MIDDLEWARE_CLASSES = (
