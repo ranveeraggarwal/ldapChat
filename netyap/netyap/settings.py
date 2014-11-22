123143dc6079b8d52786f365424ed87a7c6cc4c5
Dheerendra Rathor <dheeru.rathor14@gmail.com>
2014-11-22 16:38:25 +0530

    updated settings.py

--- netyap/netyap/settings.py
+++ netyap/netyap/settings.py
@@ -59,10 +59,10 @@ WSGI_APPLICATION = 'netyap.wsgi.application'
 DATABASES = {
    'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': 'ldapChat',                      # Or path to database file if using sqlite3.
+            'NAME': 'ldapchat',                      # Or path to database file if using sqlite3.
             # The following settings are not used with sqlite3:
-            'USER': 'ldapChat',
-            'PASSWORD': 'ldapChat',
+            'USER': 'ldapchat',
+            'PASSWORD': 'ldapchat',
             'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
             'PORT': '',                      # Set to empty string for default.
         }
