7ba715e84ee7b615e92daa267a85bd76341fd9bb
Dheerendra Rathor <dheeru.rathor14@gmail.com>
2014-11-22 19:28:47 +0530

    added authentication with cse ldap

--- netyap/authentication/urls.py
+++ netyap/authentication/urls.py
@@ -1,12 +1,13 @@
 __author__ = 'dheerendra'
 
 from django.conf.urls import patterns, include, url
-from auth import views
+from authentication import views
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'netyap.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', views.authentication, name='authentication'),
+    url(r'^$', views.index, name='index'),
+    url(r'^login', views.authentication, name='login'),
 )
--- netyap/authentication/views.py
+++ netyap/authentication/views.py
@@ -1,12 +1,24 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render, render_to_response
+from django.template import RequestContext
+import json
+from authenticator import ldapAuth
 
 # Create your views here.
 
 def authentication(request):
+    context = RequestContext(request)
     if request.method == 'POST':
         username = request.REQUEST["username"]
         password = request.REQUEST["passwd"]
         print username, password
+        authenticate = ldapAuth(username, password)
+        if authenticate == 'VALID':
+            return render_to_response('authentication/index.html', {'logged': True}, context)
+        else:
+            return render_to_response('authentication/index.html', {'logged': False}, context)
+    else:
+        return render_to_response('authentication/index.html', {'logged': 3}, context)
 
 def index(request):
-    return render_to_response('authentication/index.html', {})
+    return render_to_response('authentication/index.html', {'logged': 4}, context_instance=RequestContext(request))
