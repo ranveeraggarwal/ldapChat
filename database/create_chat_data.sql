daf65f48f876949cd20887f912cf3933fec6e2ff
nitinchandrol <ntnchandrol@gmail.com>
2014-11-26 17:57:56 +0530

    forign keys adjusted

--- database/create_chat_data.sql
+++ database/create_chat_data.sql
@@ -2,24 +2,28 @@
 --drop table if exists chat_chatroom cascade;
 --drop table if exists chat_notice cascade;
 
+delete from chat_chatroom;
+delete from chat_chat;
+delete from chat_notice;
+ALTER sequence chat_chat_chat_id_seq restart with 1 cache 1;
 
-insert into chat_chatroom(title, instructor_username, instructor_name, course_id) values
-("Lab 01 Discussion","varsha", "Varsha Apte", "CS 348"),
-("Lab 02 Discussion","varsha", "Varsha Apte", "CS 348")
-;
+insert into chat_chatroom(title, instructor_username, instructor_name, course_id, time_stamp) values
+('Lab 01 Discussion','varsha', 'Varsha Apte', 'CS 348', current_timestamp(3)),
+('Lab 02 Discussion','varsha', 'Varsha Apte', 'CS 348', current_timestamp(3));
 
-insert into chat_chat(chatroom_id, user_id, message, chat_id) values
-("1", "120050035" , "Hello World!", "-1");
 
-insert into chat_chat(chatroom_id, user_id, message) values
-("1", "120050033" , "Hello World!"),
-("1", "120050035" , "Lab01 Doubts"),
-("1", "120050020" , "Lab01 Submission issue"),
-("1", "120050035" , "Lab01 Cribs"),
-("1", "120050034" , "lab cribs");
+insert into chat_chat(chat_id, chatroom_id_id, user_id, message, parent_id_id, time_stamp) values
+('-1', '1', '120050035' , 'Hello World!', '-1', current_timestamp(3));
 
-insert into chat_notice(chatroom_id, message) values
-("1", "All cribs resolved");
+insert into chat_chat(chatroom_id_id, user_id, message, parent_id_id, time_stamp) values
+('1', '120050033' , 'Hello World!', 1, current_timestamp(3)),
+('1', '120050035' , 'Lab01 Doubts', 2, current_timestamp(3)),
+('1', '120050020' , 'Lab01 Submission issue', 3,  current_timestamp(3)),
+('1', '120050035' , 'Lab01 Cribs',4 , current_timestamp(3)),
+('1', '120050034' , 'lab cribs',5 , current_timestamp(3));
+
+insert into chat_notice(chatroom_id_id, message, time_stamp) values
+('1', 'All cribs resolved', current_timestamp(3));
 
 --insert into chat_subscribertable(chatroom_id, user_id) values
 
