642acd9dada2bc5b79bb45c1d6a47646fd503516
Ranveer Aggarwal <ranveeraggarwal@gmail.com>
2014-11-26 21:55:06 +0530

    fixed forms with ModelForms

--- netyap/chat/forms.py
+++ netyap/chat/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from models import Chatroom
 
 
-class createChatroomForm(forms.Form):
+class createChatroomForm(forms.ModelForm):
     title = forms.CharField(max_length=100, required=True)
     instructor_name = forms.CharField(max_length=100, required=True)
     course_id = forms.CharField(max_length=100, required=True)
