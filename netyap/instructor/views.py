063c7751a624d3c02f3e5b89420d705d354111d8
Dheerendra Rathor <dheeru.rathor14@gmail.com>
2014-11-26 14:52:59 +0530

    correct push

--- netyap/instructor/views.py
+++ netyap/instructor/views.py
@@ -4,11 +4,12 @@ from django.template import RequestContext
 # Create your views here.
 
 def index(request):
-    session = request.session;
+    session = request.session
     context = RequestContext(request)
     type = request.session.get('type')
     ins_data = {}
     if type == 'f':
-        ins_data['name'] =
+        ins_data['name'] = session.get('name')
+
 
     return render('/instructor/instructor_home.html', {'instructor_name': 'varsha'})
