7323c1c788b107ffcc81853fc3ab858c1107911d
Dheerendra Rathor <dheeru.rathor14@gmail.com>
2014-11-26 14:45:41 +0530

    commiting instructor_name

--- netyap/instructor/views.py
+++ netyap/instructor/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from django.template import RequestContext
 
 # Create your views here.
 
+def index(request):
+    session = request.session;
+    context = RequestContext(request)
+    type = request.session.get('type')
+    ins_data = {}
+    if type == 'f':
+        ins_data['name'] = request.
+
+    return render('/instructor/instructor_home.html', {'instructor_name': 'varsha'})
