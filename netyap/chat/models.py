79cc1f6c6e9771da1d2a2e955da1e95510c2ef68
Dheerendra Rathor <dheeru.rathor14@gmail.com>
2014-11-26 19:04:43 +0530

    capitalise model names

--- netyap/chat/models.py
+++ netyap/chat/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class chatroom(models.Model):
+class Chatroom(models.Model):
     chatroom_id = models.CharField(max_length=100, primary_key=True)
     title = models.CharField(max_length=100)
     instructor_username = models.CharField(max_length=100)
@@ -10,8 +10,8 @@ class chatroom(models.Model):
     time_stamp = models.DateTimeField(auto_now_add= True)
 
 
-class chat(models.Model):
-    chatroom_id = models.ForeignKey(chatroom)
+class Chat(models.Model):
+    chatroom_id = models.ForeignKey(Chatroom)
     chat_id = models.CharField(max_length=100, primary_key=True)
     user_id = models.CharField(max_length=100)
     message = models.CharField(max_length=500)
@@ -19,14 +19,15 @@ class chat(models.Model):
     parent_id = models.ForeignKey('self')
 
 
-class notice(models.Model):
-    chatroom_id=models.ForeignKey(chatroom)
+class Notice(models.Model):
+    chatroom_id=models.ForeignKey(Chatroom)
     notice_id = models.CharField(max_length=100, primary_key=True)
     message = models.CharField(max_length=500)
     time_stamp = models.DateTimeField(auto_now_add= True)
 
-class subscribertable(models.Model):
-    chatroom_id = models.ForeignKey(chatroom)
+
+class SubscriberTable(models.Model):
+    chatroom_id = models.ForeignKey(Chatroom)
     user_id = models.CharField(max_length=100)
 
 
