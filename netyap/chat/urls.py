a58bdf11106bc82beccc26569dc4868c3409cebd
Ranveer Aggarwal <ranveeraggarwal@gmail.com>
2014-11-26 20:29:22 +0530

    chatroom url diverted

--- netyap/chat/urls.py
+++ netyap/chat/urls.py
@@ -6,4 +6,5 @@ from chat import views
 urlpatterns = patterns('',
     url(r'^createchat$', views.createChatroom, name='createChatroom'),
     url(r'^joinchat/$', views.joinChatroom, name='joinChatroom'),
+    url(r'^(\d+)', views.index, name='index'),
 )
--- netyap/instructor/urls.py
+++ netyap/instructor/urls.py
@@ -4,5 +4,5 @@ from django.conf.urls import patterns, include, url
 import views
 
 urlpatterns = patterns('',
-    url(r'^index$', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
 )
