a4c68e3ea4ee3de0847c029518c86c46eac3d5f4
Ranveer Aggarwal <ranveeraggarwal@gmail.com>
2014-11-27 00:31:06 +0530

    stupage

--- netyap/student/views.py
+++ netyap/student/views.py
@@ -15,7 +15,14 @@ def index(request):
         student_data['username'] = session.get('username')
         student_data['userId']=session.get('userId')
         student_data['chatrooms']=showchat(student_data['userId'])
+        theCourses = []
+        for oneRoom in student_data['chatrooms']:
+            if oneRoom.course_id in theCourses:
+                pass
+            else:
+                theCourses.append(oneRoom.course_id)
         student_data['access_level']="Student"
+        student_data['courses'] = theCourses
 
     return render_to_response('student/student_home.html',student_data, context)
 
