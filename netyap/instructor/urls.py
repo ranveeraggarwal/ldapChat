fab40535d4e17474fb1620b219298cd50caf1c52
adit280595 <kabraadit@gmail.com>
2014-11-26 20:17:41 +0530

    a

--- netyap/instructor/urls.py
+++ netyap/instructor/urls.py
@@ -1,8 +1,8 @@
 __author__ = 'ranveer'
 
 from django.conf.urls import patterns, include, url
-from instructor import views
+import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
+    url(r'^index$', views.index, name='index'),
 )
--- netyap/instructor/views.py
+++ netyap/instructor/views.py
@@ -22,3 +22,4 @@ def index(request):
         redirect('/student')
     else:
         redirect('/')
+
