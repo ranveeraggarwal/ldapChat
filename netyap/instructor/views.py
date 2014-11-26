98176dac2b23804a2ecf8f89c18b19991b98c7f2
Dheerendra Rathor <dheeru.rathor14@gmail.com>
2014-11-26 19:53:05 +0530

    query resolved

--- netyap/instructor/views.py
+++ netyap/instructor/views.py
@@ -14,7 +14,10 @@ def index(request):
         ins_data['username'] = session.get('username')
         instructor_chatrooms = Chatroom.objects.filter(instructor_username=ins_data['username'])
         ins_data['chatroom'] = instructor_chatrooms
-        #notice = Notice.objects.filter(chatroom__instructor_username=ins_data['username']).order_by('time_stamp')[0]
+        try:
+            notice = Notice.objects.filter(chatroom_id__instructor_username=ins_data['username']).order_by('time_stamp')[0]
+        except IndexError:
+            notice = None
         #chatroom = notice.chatroom.title
         #ins_data['notice']
         return render_to_response('instructor/instructor_home.html', ins_data, context)
