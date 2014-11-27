b6b830ce22f46822758fb15fec6b59b64a332bb6
Ranveer Aggarwal <ranveeraggarwal@gmail.com>
2014-11-27 06:33:04 +0530

    More fixes

--- netyap/chat/views.py
+++ netyap/chat/views.py
@@ -17,7 +17,7 @@ def joinChatroom(request,chatroom_id):
     session = request.session
     user_id = session.get('username')
     theChatRoom = Chatroom.objects.all().filter(chatroom_id=chatroom_id)[0]
-    theBroadcast = Notice.objects.all().filter(chatroom_id=chatroom_id)
+    theBroadcast = Notice.objects.all().filter(chatroom_id=chatroom_id).order_by("-pk")
     chat_data = {'isSubChatRoom':0,'room_title': theChatRoom.title, 'course_name': theChatRoom.course_id, 'user_name': session.get('name'), 'instructor': theChatRoom.instructor_name}
     chat_data['broadcasts'] = theBroadcast
     subscribe= SubscriberTable(
