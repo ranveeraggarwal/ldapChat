2df8b13b6847346febf6f0fa125d6bb1096e5960
Dheerendra Rathor <dheeru.rathor14@gmail.com>
2014-11-28 14:41:02 +0530

    updated server

--- netyap/runserver.py
+++ netyap/runserver.py
@@ -6,11 +6,20 @@ import tornado.ioloop
 import tornado.web
 import tornado.wsgi
 import os
+import threading
+from datetime import datetime
+
 
 from wsChat import ChatSocketHandler
 
 
 define("port", type=int, default=8000)
+
+
+def start_server(app):
+    app.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
+
 
 def main():
     parse_command_line()
@@ -27,9 +36,11 @@ def main():
         debug=True
     )
     server = tornado.httpserver.HTTPServer(tornado_app)
-    server.listen(options.port)
-    print "server started. Press Ctrl-C to close"
-    tornado.ioloop.IOLoop.instance().start()
+    current_time = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+    message = 'Starting server at %s\n Press Ctrl-C to stop' % current_time
+    print message
+    threading.Thread(target=start_server(server)).start()
+
 
 if __name__ == '__main__':
     try:
